7e4d7d0b3259570e77e4d3c0e7718b73ba1835dd
miyakogi <miyako.dev@gmail.com>
2017-06-03 21:17:19 +0900

    Initial commit

---
+++ {{cookiecutter.project_slug}}/docs/index.rst
@@ -0,0 +1,19 @@
+Welcome to {{ cookiecutter.project_name }}'s documentation!
+======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   installation
+   usage
+   contributing
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import path
+from setuptools import setup
+
+basedir = path.dirname(path.abspath(__file__))
+with open(path.join(basedir, 'README.rst')) as readme_file:
+    readme = readme_file.read()
+
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme,
+
+    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+
+    packages=[
+        '{{ cookiecutter.project_slug }}',
+    ],
+    package_dir={'{{ cookiecutter.project_slug }}':
+                 '{{ cookiecutter.project_slug }}'},
+    include_package_data=True,
+    install_requires=requirements,
+
+    license="{{ cookiecutter.license }}",
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+{%- if cookiecutter.license in license_classifiers %}
+        '{{ license_classifiers[cookiecutter.license] }}',
+{%- endif %}
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
