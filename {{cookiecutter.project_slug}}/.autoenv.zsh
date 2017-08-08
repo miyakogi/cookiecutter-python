b2ea9672b3a3348b7f4ec45edd48729ea8751a69
miyakogi <miyako.dev@gmail.com>
2017-08-09 05:51:23 +0900

    Use `workon` if possible on autoenv

--- {{cookiecutter.project_slug}}/.autoenv.zsh
+++ {{cookiecutter.project_slug}}/.autoenv.zsh
@@ -1,7 +1,11 @@
 autostash PYVENV_NAME={{ cookiecutter.project_slug }}
 autostash PROJECT_ROOT=${HOME}/Projects/{{ cookiecutter.project_slug }}
-[[ -e ${HOME}/.pyvenv/{{ cookiecutter.project_slug }}/bin/activate ]] && \
-  echo "Activate Venv (${PYVENV_NAME})" && \
-  source ${HOME}/.pyvenv/{{ cookiecutter.project_slug }}/bin/activate
+if type workon >/dev/null 2>&1; then
+  workon ${PYVENV_NAME} && echo "Activate Venv (${PYVENV_NAME})"
+else
+  [[ -e ${PYVENV_DIR:-$HOME/.pyvenv}/${PYVENV_NAME}/bin/activate ]] && \
+    echo "Activate Venv (${PYVENV_NAME})" && \
+    source ${PYVENV_DIR:-$HOME/.pyvenv}/${PYVENV_NAME}/bin/activate
+fi
 autostash PYTHONPATH=$PROJECT_ROOT${PYTHONPATH:+:}$PYTHONPATH
 autostash MYPYPATH=$PROJECT_ROOT
