2e6557628661fcd12957730fff16d3d70b95df90
miyakogi <miyako.dev@gmail.com>
2017-08-27 19:28:50 +0900

    Change venv path

--- {{cookiecutter.project_slug}}/.autoenv.zsh
+++ {{cookiecutter.project_slug}}/.autoenv.zsh
@@ -1,11 +1,7 @@
 autostash PYVENV_NAME={{ cookiecutter.project_slug }}
 autostash PROJECT_ROOT=${HOME}/Projects/{{ cookiecutter.project_slug }}
-if type workon >/dev/null 2>&1; then
-  workon ${PYVENV_NAME} && echo "Activate Venv (${PYVENV_NAME})"
-else
-  [[ -e ${PYVENV_DIR:-$HOME/.pyvenv}/${PYVENV_NAME}/bin/activate ]] && \
-    echo "Activate Venv (${PYVENV_NAME})" && \
-    source ${PYVENV_DIR:-$HOME/.pyvenv}/${PYVENV_NAME}/bin/activate
-fi
+[[ -e ${PROJECT_ROOT}/bin/activate ]] && \
+  echo "Activate Venv (${PYVENV_NAME})" && \
+  source ${PROJECT_ROOT}/bin/activate
 autostash PYTHONPATH=$PROJECT_ROOT${PYTHONPATH:+:}$PYTHONPATH
 autostash MYPYPATH=$PROJECT_ROOT
