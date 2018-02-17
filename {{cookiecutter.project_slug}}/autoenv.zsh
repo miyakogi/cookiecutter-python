f2b7daccaaee4d06e1e9da6cd3704f384d2b0fec
miyakogi <miyako.dev@gmail.com>
2018-02-17 17:18:27 +0900

    Use unittest as default test runner

--- {{cookiecutter.project_slug}}/autoenv.zsh
+++ {{cookiecutter.project_slug}}/autoenv.zsh
@@ -2,4 +2,4 @@ autostash PYVENV_NAME={{ cookiecutter.project_slug }}
 autostash PROJECT_ROOT=${HOME}/Projects/{{ cookiecutter.project_slug }}
 workon $PYVENV_NAME
 autostash MYPYPATH=$PROJECT_ROOT
-autostash VIM_PYTHON_TEST="green"
+autostash VIM_PYTHON_TEST="unittest"
