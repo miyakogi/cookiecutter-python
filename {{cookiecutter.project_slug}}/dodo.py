712da8a3a977cb4aace8c12bc98e3be1bf96a51c
miyakogi <miyako.dev@gmail.com>
2018-09-15 18:50:38 +0900

    Add spelling check on doit

--- {{cookiecutter.project_slug}}/dodo.py
+++ {{cookiecutter.project_slug}}/dodo.py
@@ -53,11 +53,21 @@ def task_readme():
     }
 
 
+def task_spell():
+    """Run spell chek."""
+    return {
+        'actions': [
+            'pylint --disable all --enable spelling --spelling-dict en_US '
+            '--spelling-private-dict-file spell.txt pyppeteer'
+        ],
+    }
+
+
 def task_check():
     """Run flake8/mypy/pydocstyle/docs tasks."""
     return {
         'actions': None,
-        'task_dep': ['flake8', 'mypy', 'pydocstyle', 'docs', 'readme']
+        'task_dep': ['flake8', 'mypy', 'pydocstyle', 'docs', 'readme', 'spell']
     }
 
 
